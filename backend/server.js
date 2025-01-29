bda4da77879d00c76d6e701701e587a6fdbe19e2
Rithin5466 <chegondirithinsurya@gmail.com>
2025-01-29 19:16:58 +0530

    ntg

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Rithin!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
